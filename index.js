5656f48c1ae3ebe03c9269707e10d01ed306af3b
danielmartellini <danielbmartellini@gmail.com>
2022-01-05 19:49:50 +0000

    cahanges to index

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const router = express(),
 server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname,'files'))); //serves static content from "files" folder
+router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
+router.use(express.json()); //We include support for JSON
+
+
+function XMLtoJSON(filename, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+      if (err) throw (err);
+      xml2js.parseString(xmlStr, {}, cb);
+    });
+};
+  
+  //Function to convert JSON to XML and save it
+function JSONtoXML(filename, obj, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+};
 
 router.get('/get/html', function(req, res){
 
