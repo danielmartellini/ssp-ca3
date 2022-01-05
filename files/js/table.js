bf9e02d26440b47cf536aba93fef11af83e8f25a
danielmartellini <danielbmartellini@gmail.com>
2022-01-05 19:41:29 +0000

    trying to display table

--- files/js/table.js
+++ files/js/table.js
@@ -1,21 +1,26 @@
-function import_table(){
-    $("#results").empty();
-    $.getHTMLuncached = function(url){
-        return $.ajax({
-            url:url,
-            type: 'GET',
-            cache: false,
-            success: function (html){
-                $("#results").append(html);
-            }
-        })
-    }
-    $.getHTMLuncached("/get/html");
-}
 
-$(document).ready(function(){
-
-    import_table();
-})
+function draw_table()
+{
+	$("#results").empty();
+	$.getJSONuncached = function (url)
+	{
+		return $.ajax(
+		{
+			url: url,
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#results").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/get/html")
+};
+$(document).ready(function ()
+{
+	draw_table();
+});
 
 
